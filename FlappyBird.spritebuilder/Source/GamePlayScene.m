22fc1f5d8f3ff70afba01082edc0668fba53f92f
Cameron Talley <camerontalley@Cameron-Talleys-MacBook-Pro-15.local>
2014-10-25 10:56:42 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,24 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //Increment the time sinc the last obstacle was added
+    timeSinceObstacle += delta;  //delta is approx. 1/60
+    
+    //check if two seconds have passed
+    
+    if (timeSinceObstacle > 2.0f)
+    {
+        //add a new obstacle
+        [self addObstacle];
+        //reset timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
